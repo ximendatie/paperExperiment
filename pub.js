96ddddeb1a60c0184370ad9b6c75c5f37edc4ea1
harvey <heweimr@gmail.com>
2017-06-10 00:27:24 +0800

    循环发请求，算平均网络时间和运算时间

--- pub.js
+++ pub.js
@@ -9,14 +9,25 @@ client.on('connect', function () {
     fs.readFile('./raw.md',function(err,data){
         if(err) throw err;
 
-        // 发送原始数据
-        var pubData = '{"sendTime":'+new Date().getTime()+','+'"data":'+data.toString()+'}';
-        if(data.length>0){
-            client.publish('ICS',pubData);
-            console.log('pubed');
-        }          
-        else
-            console.log('null?');
+        var i = 1;
+        var timer = setInterval(function(){
+
+            // 发送原始数据
+            var pubData = '{"sendTime":'+new Date().getTime()+','+'"data":'+data.toString()+'}';
+            if(data.length>0){
+                client.publish('ICS',pubData);
+                console.log('pubed');
+            }          
+            else
+                console.log('null?');
+            //循环10次
+            if(i == 10){
+                clearInterval(timer);
+            }
+            i++;
+        },300);
+
+        
     });
 
 
--- sub.js
+++ sub.js
@@ -3,6 +3,8 @@ var fs = require('fs');
 
 var Sclient = mqtt.connect('mqtt://127.0.0.1'); 
 
+var timeResult = new Array();
+
 
 Sclient.on('connect', function () {
   Sclient.subscribe('ICS');
@@ -23,10 +25,28 @@ Sclient.on('message', function (topic, message) {
       // 处理后存储
       if(i === length-1){
         messObj.resultTime = new Date().getTime();
-        fs.writeFile('./result.md',JSON.stringify(messObj),function(err){
-                if(err) throw err;
-                console.log('has finished');
-        });
+        console.log('\ncloud solve');
+        console.log(messObj.sendTime+'~'+messObj.getTime+'~'+messObj.resultTime);
+        console.log('network:'+(messObj.getTime-messObj.sendTime)+'ms'+'\nsolve:'+(messObj.resultTime-messObj.getTime)+'ms');
+
+        var temp = new Object();
+        temp.netTime = messObj.getTime-messObj.sendTime;
+        temp.solTime = messObj.resultTime-messObj.getTime;
+        timeResult.push(temp);
+
+        if(timeResult.length === 10){
+          var a=0,b=0;
+          for(var i =0; i < 10; i++){
+            a+=timeResult[i].netTime;
+            b+=timeResult[i].solTime;
+          }
+          console.log('ave:'+a/10+','+b/10);
+        }
+
+        // fs.writeFile('./result.md',JSON.stringify(messObj),function(err){
+        //         if(err) throw err;
+        //         console.log('has finished');
+        // });
       }
     } 
 });
