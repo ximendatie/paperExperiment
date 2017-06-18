d0bd1b59f5477116f52610bc347f02d67789a0b9
harvey <heweimr@gmail.com>
2017-06-18 22:33:36 +0800

    修改local

--- pub-local.js
+++ pub-local.js
@@ -8,54 +8,32 @@ client.on('connect', function () {
 
     fs.readFile('./raw.md',function(err,data){
         if(err) throw err;
-
-        
-        var c = 1;
+        var c = 1
+        var pubData = data.toString();
+        console.log('start time and send time:');
         var timer = setInterval(function(){
-            var pubData = new Object();
-            pubData.startTime = new Date().getTime();
-            var dataObj = JSON.parse(data);
-
+            console.log(new Date().getTime()) ;
             // 模拟数据处理  
-            var length = dataObj.length; 
-            var newData = [];
-            for(var i = 0, j = 0; i<length; i++){
+            var length = pubData.length; 
+            var result = '';
+            for(var i = 0; i < length; i++){
                 //处理数据
-                if(dataObj[i].tem<1 && dataObj[i].hum<1){
-                    newData[j++] = dataObj[i];
-                }
-                // 处理后存储
-                if(i === length-1){
-                    console.log('i:'+i);
-                    console.log('\n local solve');
-                    pubData.resultTime = new Date().getTime();
-                    pubData.data = newData;
-                    console.log('data length ' + length + 'newData length '+ newData.length);      
-                    console.log(pubData.startTime+'~'+pubData.resultTime);
-                    console.log('\nsolve:'+(pubData.resultTime-pubData.startTime)+'ms');
-
-
-                    if(newData.length>0){
-                        client.publish('ICS',JSON.stringify(pubData) );
-                        console.log(JSON.stringify(pubData));
-                        console.log('pubed');
-                    }          
-                    else
-                        console.log('null?');
+                if(pubData[i] === 't'){
+                    result+='t';
+                }  
+                if(i == length-1){
+                    console.log(new Date().getTime());
                     
-                    // fs.writeFile('./result.md',JSON.stringify(messObj),function(err){
-                    //         if(err) throw err;
-                    //         console.log('has finished');
-                    // });
-                }
+                    client.publish('ICS',result);
+                }               
             } 
 
-            //循环20次
-            if(c == 20){
+            //循环5次
+            if(c == 5){
                 clearInterval(timer);
             }
             c++;
-        },300);
+        },10000);
 
         
     });
--- sub-local.js
+++ sub-local.js
@@ -8,41 +8,12 @@ var Sclient = mqtt.connect('mqtt://127.0.0.1');
 
 Sclient.on('connect', function () {
   Sclient.subscribe('ICS');
+  console.log('receive result:')
 });
 Sclient.on('message', function (topic, message) {
-    var getTime = new Date().getTime();
-    var messObj = JSON.parse(message);
-    messObj.getTime = getTime;
-
-
-
-    // 处理后存储
-
-    console.log('\ncloud solve');
-    messObj.resultTime = new Date().getTime();
-    console.log('data length ' + messObj.length );      
-    console.log(messObj.startTime+'~'+messObj.resultTime+'~'+messObj.getTime);
-    console.log('network:'+(messObj.getTime-messObj.resultTime)+'ms'+'\nsolve:'+(messObj.resultTime-messObj.startTime)+'ms');
-
-    // var temp = new Object();
-    // temp.netTime = messObj.getTime-messObj.startTime;
-    // temp.solTime = messObj.resultTime-messObj.getTime;
-    // timeResult.push(temp);
-
-    // if(timeResult.length === 20){
-    //   var a=0,b=0;
-    //   for(var i =0; i < 10; i++){
-    //     a+=timeResult[i].netTime;
-    //     b+=timeResult[i].solTime;
-    //   }
-    //   console.log('ave:'+a/10+','+b/10);
-    // }
-
     
-    // fs.writeFile('./result.md',JSON.stringify(messObj),function(err){
-    //         if(err) throw err;
-    //         console.log('has finished');
-    // });
+    console.log(new Date().getTime());
+// console.log(message.toString());
     
 });
 
