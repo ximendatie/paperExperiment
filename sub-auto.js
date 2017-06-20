ca693aed2c8d1441658a04a6ea1dedc72c82e97a
harvey <heweimr@gmail.com>
2017-06-21 00:48:34 +0800

    -

--- sub-auto.js
+++ sub-auto.js
@@ -11,20 +11,25 @@ client.on('connect', function () {
     client.subscribe('ICS3');
     client.subscribe('ICS4');
     client.subscribe('ICS5');
+    client.subscribe('ICS6');
+    client.subscribe('ICS7');
+    client.subscribe('ICS8');
+    client.subscribe('ICS9');
     
 });
   
 client.on('message', function (topic, message) {
 
-    var num = parseInt(topic);
-    console.log(topic+' received:' + new Date().getTime());
+    var num = parseInt(topic.substring(3));
+    console.log('\n' + topic+' received:' + new Date().getTime());
     message = message.toString();
     var len = message.length;
-    var result=message.substr(len/2);
-    for(var i = 0; i < len/2; i++){
-        if(i === len/2 - 1){
+    var result=message.substr(parseInt(len/2));
+    console.log(len);
+    for(var i = 0; i < parseInt(len/2); i++){
+        if(i === parseInt(len/2) - 1){
             console.log(topic+' solved:' +new Date().getTime());//处理结束时间
-            client.publish('topic'+nun,result);
+            client.publish('ICS'+(num+1),result);
         }  
     }
 
