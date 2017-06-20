fb8fca26116f34c71d7813744a9ef6eeb9754e23
He Wei <602710492@qq.com>
2017-06-20 16:53:06 +0800

    Update sub-auto.js
    多功能模块

--- sub-auto.js
+++ sub-auto.js
@@ -1,28 +1,32 @@
-var mqtt = require('mqtt');
+var mqtt    = require('mqtt');
 var fs = require('fs');
 
-var Sclient = mqtt.connect('mqtt://127.0.0.1'); 
+var client  = mqtt.connect('mqtt://127.0.0.1');
 
+ 
+client.on('connect', function () {
 
-Sclient.on('connect', function () {
-  Sclient.subscribe('ICS');
-  console.log('receive time and result time (20):');
+    client.subscribe('ICS1');
+    client.subscribe('ICS2');
+    client.subscribe('ICS3');
+    client.subscribe('ICS4');
+    client.subscribe('ICS5');
+    
 });
-Sclient.on('message', function (topic, message) {
+  
+client.on('message', function (topic, message) {
 
-    console.log(new Date().getTime());//接受数据时间
+    var num = parseInt(topic);
+    console.log(topic+' received:' + new Date().getTime());
     message = message.toString();
-    var result='';
-    for(var i = 0; i < message.length; i++){
-      if(message[i] === 't'){
-        result+='t';
-      }
-      if(i === message.length - 1){
-        console.log(new Date().getTime());//处理结束时间
-      }
-        
+    var len = message.length;
+    var result=message.substr(len/2);
+    for(var i = 0; i < len/2; i++){
+        if(i === len/2 - 1){
+            console.log(topic+' solved:' +new Date().getTime());//处理结束时间
+            client.publish('topic'+nun,result);
+        }  
     }
-    
-    
+
 });
 
